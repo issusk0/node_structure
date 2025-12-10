c5276e681aab0d659f415f304df5e5dc4a682fd1
ubuntu-miku <plazasjolifenzio@gmail.com>
2025-12-10 17:28:06 -0300

    progress

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,6 +2,7 @@ mod utils;
 use utils::{ask_data, Node};
 use std::thread;
 use std::sync::{Arc, Mutex};
+//Arc<Mutex<Dato>> para manejar un mismo dato con varios hilos distintos controlando el flujo y integridad de dato * hilo
 fn main() {
     let shared_data: Arc<Mutex<Vec<Box<Node>>>> = Arc::new(Mutex::new(Vec::new()));
     let producer_nodes= Arc::clone(&shared_data);
